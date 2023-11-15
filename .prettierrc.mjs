030fa2a3b053923a6919a3223fb07deb207668c9
David Abell <79927957+david-abell@users.noreply.github.com>
2023-11-15 12:28:27 +0000

    add prettier & broken jsx

---
+++ .prettierrc.mjs
@@ -0,0 +1,18 @@
+/** @type {import("prettier").Config} */
+export default {
+  plugins: ["prettier-plugin-astro"],
+  overrides: [
+    {
+      files: "*.astro",
+      options: {
+        parser: "astro",
+      },
+    },
+  ],
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  printWidth: 120,
+  plugins: ["prettier-plugin-astro"],
+  pluginSearchDirs: false,
+};
